42aaae4cbf5c39f578e680ecfb70f8da81197e94
IT19179018 <it19179018@my.sliit.lk>
2022-10-26 12:44:36 +0530

    Login and Registration

--- app/src/main/java/com/example/fuel/stationOwnerLogin/StationOwner_SignUpFragment.java
+++ app/src/main/java/com/example/fuel/stationOwnerLogin/StationOwner_SignUpFragment.java
@@ -1,5 +1,6 @@
 package com.example.fuel.stationOwnerLogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,8 +11,14 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.fuel.DBHelper;
+import com.example.fuel.Login_SignUp_Interface;
+import com.example.fuel.MainActivity;
 import com.example.fuel.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -66,6 +73,7 @@ public class StationOwner_SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+//        Initializing Dropdown values for Station Location and Station Type in Signup
         String[] LocationList = {"Dehiwala", "Kalubowila", "Ratmalana"};
         String[] TypeList = {"IOC", "Ceypetco"};
         AutoCompleteTextView stationLocation;
@@ -84,24 +92,66 @@ public class StationOwner_SignUpFragment extends Fragment {
         stationLocation.setAdapter(adapterStationLocations);
         stationType.setAdapter(adapterStationTypes);
 
+
+        final String[] stationLocationValue = new String[1];
+
         stationLocation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                String item = adapterView.getItemAtPosition(position).toString();
-//                Toast.makeText(getActivity(), "Item: " + item, Toast.LENGTH_SHORT).show();
+                stationLocationValue[0] = adapterView.getItemAtPosition(position).toString();
+//                Toast.makeText(getActivity(), "Item: " + stationType, Toast.LENGTH_SHORT).show();
             }
         });
+
+        final String[] stationTypeValue = new String[1];
 
         stationType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                String item = adapterView.getItemAtPosition(position).toString();
-//                Toast.makeText(getActivity(), "Item: " + item, Toast.LENGTH_SHORT).show();
+                stationTypeValue[0] = adapterView.getItemAtPosition(position).toString();
+//               Toast.makeText(getActivity(), "Item: " + stationType, Toast.LENGTH_SHORT).show();
             }
         });
 
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_sign_up2, container, false);
+
+
+//        Login Field Initialization
+
+        TextInputLayout stationName = v.findViewById(R.id.enterStationName);
+        TextInputLayout stationEmail = v.findViewById(R.id.enterStationEmail);
+        TextInputLayout password = v.findViewById(R.id.enterStationPassword);
+        Button signUpButton = v.findViewById(R.id.signUpBtn);
+        DBHelper DB = new DBHelper(getActivity());
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String station_Name = stationName.getEditText().getText().toString();
+                String station_Email = stationEmail.getEditText().getText().toString();
+                String pass = password.getEditText().getText().toString();
+                String station_Location = String.valueOf(stationLocationValue);
+                String station_Type = String.valueOf(stationTypeValue);
+
+                Boolean checkExistingUser = DB.checkExistingUser(station_Email, pass);
+
+                if (station_Name.equals("") || pass.equals("") || station_Email.equals("") || station_Location.equals("") || station_Type.equals("")) {
+                    Toast.makeText(getActivity(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else if (checkExistingUser == false) {
+                    Boolean registerUser = DB.insertData(station_Email, station_Email, pass, station_Location, station_Type);
+                    if (registerUser == true) {
+                        Toast.makeText(getActivity(), "You have successfully registered", Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin = new Intent(getActivity(), Login_SignUp_Interface.class);
+                        startActivity(moveToLogin);
+                    } else {
+                        Toast.makeText(getActivity(), "Registration Unsuccessful", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getActivity(), "User already exists!", Toast.LENGTH_SHORT).show();
+                }
+            }});
 
         return v;
 
